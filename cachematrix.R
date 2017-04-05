a0d52184843ae1fd28dd52078795cbe78f75cda9
RAMA KRISHNA RAGHAVENDRA BULUSU <brk.raghavendra@gmail.com>
2017-04-05 11:34:22 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+## Function Definition by passing Matrix as an arguement
+{
+  mtx <- NULL           ##Set Matrix to Null
+  set <- function(y)    ##Set Function 
+  {
+    x <<- y            ##Assign Value Y to Object X
+    mtx <<- NULL       ##Set Matrix to Null
+  }
+  get <- function() x ##Get Matrix x
+  setinverse <- function(solve) mtx <<- solve   ##Set the inverse Matrix
+  getinverse <- function() mtx                  ##Get the Inverse of matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)##Construct,Coerce and check for both kinds in list 
 }
 
 
+
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) 
+  {
+    mtx <- x$getinverse()     ##Get inverse of matrix vector
+    if(!is.null(mtx))         ##Checking for Not Null Matrix
+      { 
+      message("getting cached data")  ##If Not Null Display the message
+      return(mtx)                     ##Return the Cached Data matrix
+      }
+    data <- x$get()                   ##Get matrix from x and assign it to Data
+    mtx <- solve(data, ...)           ##Assining the solved matrix to mtx matrix
+    x$setinverse(mtx)                 ##Inversion the the matrix
+    ## Return a matrix that is the inverse of 'x'
+    mtx
+  }      
